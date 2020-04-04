4ccd37be90664fa41b7c087caf2753dc4d89fded
Binod Mainali <binodmainali1@gmail.com>
2020-04-04 22:20:25 +0530

    Refactored code

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,14 +1,5 @@
 module EventsHelper
-  def multi_day_event(event)
-    event.start_date.present? && event.end_date.present?
-  end
-
-  def discounted_price(event)
-    if event.event_pricing.present?
-      price = event.event_pricing.price
-      (price - price * 5/100)
-    else
-      "Free"
-    end
+  def time_format time
+    time.strftime("%B %d, %Y")
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,17 @@ class Event < ApplicationRecord
   def ticket_price
     event_pricing.present? ? event_pricing.price : 'Free'
   end
+
+  def multi_day_event?
+    start_date.present? && end_date.present?
+  end
+
+  def discounted_price
+    if event_pricing.present?
+      price = event_pricing.price
+      (price - price * 5/100)
+    else
+      "Free"
+    end
+  end
 end
